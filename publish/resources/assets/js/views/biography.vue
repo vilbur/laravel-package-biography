6966b5d7be3bbe1532625d24dbb69eb7e2a3847c
Unknown <cerny.vil@gmail.com>
2018-02-13 06:04:59 +0100

    update

--- publish/resources/assets/js/views/biography.vue
+++ publish/resources/assets/js/views/biography.vue
@@ -1,18 +1,19 @@
 <template>
-	<div>
-		<div v-for="(model, index) in models" class="hero bio"  >
+	<div id="bio">
+		<div v-for="(model, index) in models" class="hero"  >
+			<div class="hero-body">
+				<div class="columns">
+					<div class="column is-4 has-text-centered">
+						<img class="bio_logo" v-bind:src="model.image">
+					</div>
+					<div class="column">
+						<h3 class="title is-3 is-left">{{ model.client }}</h3>
+						<h4 class="subtitle is-4 is-left">{{ model.since_until }}</h4>
+						<h5 class="subtitle is-5 is-left">{{ model.job_title }}</h5>
+					</div>
+				</div>
 
-			<div class="columns">
-				<div class="column is-4 has-text-centered">
-					<img class="bio_img" v-bind:src="model.image">
-				</div>
-				<div class="column">
-					<h3 class="title is-3 is-left">{{ model.client }}</h3>
-					<h4 class="subtitle is-4 is-left">{{ model.since_until }}</h4>
-					<h5 class="subtitle is-5 is-left">{{ model.job_title }}</h5>
-				</div>
 			</div>
-
 		</div>
 	</div>
 </template>
